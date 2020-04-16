4e862a3dccfa9980b7711c19758727785748a1f9
Bluemi <bruno.schilling@protonmail.ch>
2020-04-16 17:59:21 +0200

    added some countries; bicycles -> motor vehicles

--- Main.gd
+++ Main.gd
@@ -23,35 +23,38 @@ class Country:
 	var name
 	var vegetarians
 	var nuclear_power
-	var bicycles
+	var motor_vehicles
 
-	func _init(name_arg, vegetarians_arg, nuclear_power_arg, bicycles_arg):
+	func _init(name_arg, vegetarians_arg, nuclear_power_arg, motor_vehicles_arg):
 		self.name = name_arg
 		self.vegetarians = vegetarians_arg
 		self.nuclear_power = nuclear_power_arg
-		self.bicycles = bicycles_arg
+		self.motor_vehicles = motor_vehicles_arg
 
-# Country	Percentage Vegetarians	Nuclear Power	Bicycle Drivers
+# Country	Percentage Vegetarians	Nuclear Power	Motor Vehicles per capita
 var countries = [
-	Country.new("China", 0.04, 0.04, 0.37),
-	Country.new("Belgium", 0.07, 0.39, 0.48),
-	Country.new("Switzerland", 0.14, 0.36, 0.49),
-	Country.new("Japan", 0.04, 0.06, 0.57),
-	Country.new("Finland", 0.02, 0.32, 0.6),
+	Country.new("China", 0.04, 0.04, 0.18),
+	Country.new("Belgium", 0.07, 0.39, 0.5),
+	Country.new("Switzerland", 0.14, 0.36, 0.71),
+	Country.new("Japan", 0.04, 0.06, 0.61),
+	Country.new("Finland", 0.02, 0.32, 0.75),
 	Country.new("Norway", 0.04, 0.0, 0.61),
-	Country.new("Sweden", 0.1, 0.4, 0.64),
-	Country.new("Germany", 0.1, 0.1, 0.75),
-	Country.new("Denmark", 0.05, 0.0, 0.8),
-	Country.new("Netherlands", 0.05, 0.03, 0.99),
-	Country.new("India", 0.31, 0.03, 0.45),
-	Country.new("USA", 0.06, 0.19, 0.32),
-	Country.new("Brazil", 0.14, 0.02, 0.07),
-	Country.new("Israel", 0.13, 0.0, 0.13),
-	Country.new("Australia", 0.12, 0.0, 0.55),
-	Country.new("NewZealand", 0.01, 0.0, 0.21),
-	Country.new("Mexico", 0.08, 0.05, 0.03),
-	Country.new("Russia", 0.03, 0.17, 0.25),
-	Country.new("Chile", 0.06, 0.0, 0.23)
+	Country.new("Sweden", 0.1, 0.4, 0.51),
+	Country.new("Germany", 0.1, 0.1, 0.56),
+	Country.new("Denmark", 0.05, 0.0, 0.43),
+	Country.new("Netherlands", 0.05, 0.03, 0.48),
+	Country.new("India", 0.31, 0.03, 0.022),
+	Country.new("USA", 0.06, 0.19, 0.83),
+	Country.new("Brazil", 0.14, 0.02, 0.35),
+	Country.new("Israel", 0.13, 0.0, 0.38),
+	Country.new("Australia", 0.12, 0.0, 0.73),
+	Country.new("NewZealand", 0.01, 0.0, 0.86),
+	Country.new("Mexico", 0.08, 0.05, 0.29),
+	Country.new("Russia", 0.03, 0.17, 0.38),
+	Country.new("Chile", 0.06, 0.0, 0.23),
+	Country.new("Canada", 0.09, 0.16, 0.68),
+	Country.new("France", 0.05, 0.71, 0.47),
+	Country.new("Spain", 0.01, 0.2, 0.64),
 ]
 
 var _selected_id: int = -1
@@ -95,10 +98,10 @@ func show_nuclear_power():
 		percentages.append(country.nuclear_power)
 	_show_percentage(percentages, Color.white, Color(1.0, 0.0, 0.0))
 
-func show_bicycle_drivers():
+func show_motor_vehicles():
 	var percentages = []
 	for country in countries:
-		percentages.append(country.bicycles)
+		percentages.append(country.motor_vehicles)
 	_show_percentage(percentages, Color.white, Color.indigo)
 	
 func _on_popup_id_pressed(id: int):
@@ -107,5 +110,5 @@ func _on_popup_id_pressed(id: int):
 		match _selected_id:
 			0: show_vegetarians()
 			1: show_nuclear_power()
-			2: show_bicycle_drivers()
+			2: show_motor_vehicles()
 		$MenuButton.text = $MenuButton.get_popup().get_item_text(id)
